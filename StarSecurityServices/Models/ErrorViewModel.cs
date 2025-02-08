7d9b8578ddc123ab67a7884a5fceb800153ad8e6
my <my@DESKTOP-8T25EO0>
2025-02-08 13:48:32 +0500

    Add project files.

---
+++ StarSecurityServices/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StarSecurityServices.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
